7edd0bb9df8b227eeb26b01f34b3e541894b8c18
MHD-AKEEL <mhdakeel7755@gmail.com>
2025-09-15 14:20:52 +0300

    modified Arithmetic file

--- Java/Arithmetic.java
+++ Java/Arithmetic.java
@@ -2,15 +2,28 @@ class Arithmetic {
 
     public static void main(String[] args) {
 
-        int x = 8, y = 5;
+        int x = 4, y = 8, i = 4;
 
-        System.out.println("x=" + x);
+        /*System.out.println("x=" + x);
         System.out.println("y=" + y);
         System.out.println("x+y=" + x + y);
         System.out.println("x-y=" + x - y);
         System.out.println("x*y=" + x * y);
         System.out.println("x/y=" + x / y);
-        System.out.println("x%y=" + x % y);
+        System.out.println("x%y=" + x % y);*/
+
+	//AND
+	System.out.println(i<=8 && y>=6);
+
+	//OR
+	System.out.println(i<=5 || y>=5);
+
+	//NOT
+	System.out.println(!(i>5)); 
+	System.out.println(!(i>=3));
+
+	System.out.println(!(i>=2 && y>=2));
+
     }
 }
 
