07d07ee0d935002c397da6c7e3ff9af139f8a10e
MHD-AKEEL <mhdakeel7755@gmail.com>
2025-09-22 00:26:45 +0300

    modified Java/BIT.java and  deleted Java/java

--- Java/BIT.java
+++ Java/BIT.java
@@ -1,5 +1,18 @@
 public class BIT {
+
 	public static void main(String[] args) {
 	System.out.println("Welcome to Java");
+
+byte num1=127;
+byte num2=22;
+
+int total;
+
+num1++;
+++num2;
+
+
+System.out.println("Total="+num1);
+
     }
 }
