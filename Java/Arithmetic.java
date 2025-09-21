3be0ff2b2921290a6aac32f62d8eb03f89d022d8
MHD-AKEEL <mhdakeel7755@gmail.com>
2025-09-21 15:22:23 +0300

    modified Arithmetic.class and Arithmetic.java

--- Java/Arithmetic.java
+++ Java/Arithmetic.java
@@ -14,13 +14,19 @@ class Arithmetic {
 
 	//AND
 	System.out.println(i<=8 && y>=6);
+	System.out.println(x<=5 && y>=7);
+	System.out.println(y<=10 && i>=5);
 
 	//OR
 	System.out.println(i<=5 || y>=5);
+	System.out.println(y<=9 || x>=2);
+	System.out.println(x<=3 || x>=6);
 
 	//NOT
 	System.out.println(!(i>5)); 
 	System.out.println(!(i>=3));
+	System.out.println(!(x<=6));
+	System.out.println(!(y<=4));
 
 	System.out.println(!(i>=2 && y>=2));
 
