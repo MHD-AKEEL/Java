7fb7f1afe8062305c2e7c0de47de3422664e399c
MHD-AKEEL <mhdakeel7755@gmail.com>
2025-09-22 22:49:48 +0300

    modified Java/BIT.java

--- Java/BIT.java
+++ Java/BIT.java
@@ -4,11 +4,21 @@ public class BIT {
 	System.out.println("Welcome to Java");
 
 
-float num1=12.0f;
+float num2=12.0f;
+
+int num1=010;
+int num3=021;
+int num4=0b100;
+float num5=010;
+
+int num6=0x10;
 
 
 
 System.out.println("num1="+num1);
-
+System.out.println("num3="+num3);
+System.out.println("num4="+num4);
+System.out.println("num5="+num5);
+System.out.println("num6="+num6);
     }
 }
