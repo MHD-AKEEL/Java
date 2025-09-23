6cde37b1f9444245bb5c8c0c1711708df18d7858
MHD-AKEEL <mhdakeel7755@gmail.com>
2025-09-23 23:30:51 +0300

    created test.java

---
+++ Java/test.java
@@ -0,0 +1,8 @@
+public class test{
+	public static void main (String[] args){
+
+	System.out.println("Welcome To Java Programming");
+	System.out.println("Welcome \n To Java \n Programming");
+}
+
+}
