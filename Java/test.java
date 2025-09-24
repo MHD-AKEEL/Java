8902a4a33a6d34d7ef92b0554303c0fb05e10248
MHD-AKEEL <mhdakeel7755@gmail.com>
2025-09-25 00:44:29 +0300

    created test2.java and modified test.java

--- Java/test.java
+++ Java/test.java
@@ -1,8 +1,27 @@
-public class test{
+public class test {
 	public static void main (String[] args){
 
-	System.out.println("Welcome To Java Programming");
-	System.out.println("Welcome \n To Java \n Programming");
+	int a=5;
+	int b=4;
+
+	int c=a&b;
+	int d=a|b;
+
+int num1=5;
+int num2=2;
+int num3=7;
+
+System.out.println(num1>8 && num2>1);
+System.out.println(num1>8 || num2>1);	
+
+System.out.println(num1>8 & num2>1);
+System.out.println(num1>8 | num2>1);
+
+	System.out.println(c);
+	System.out.println(d);
+
+num3=4^3;
+System.out.println(num3);
 }
 
 }
