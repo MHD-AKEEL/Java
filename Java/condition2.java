63b7b6c9e652bd6576dc6e732cfd565bd48417c4
MHD-AKEEL <mhdakeel7755@gmail.com>
2025-09-29 15:47:41 +0300

    created condition2.java

---
+++ Java/condition2.java
@@ -0,0 +1,27 @@
+public class condition2{
+
+public static void main(String args[]){
+
+char c='u';
+
+switch(c)
+{
+	case 'a':System.out.println("Vowel-A");
+	break;
+
+	case 'e':System.out.println("Vowel-E");
+	break;
+
+	case 'i':System.out.println("Vowel-I");
+	break;
+
+	case 'o':System.out.println("Vowel-O");
+	break;
+
+	case 'u':System.out.println("Vowel-U");
+	break;
+	default:System.out.println("Not Vowel");
+}
+
+}
+}
