74b200ce3f9204f63c1c601139034b4983938ddd
MHD-AKEEL <mhdakeel7755@gmail.com>
2025-09-30 23:35:55 +0300

    created testwhile.java and modified testloop.java

--- Java/testloop.java
+++ Java/testloop.java
@@ -1,7 +1,20 @@
 public class testloop{
 public static void main(String args[]){
-	for(int i=0;i<5;i++)
+	for(int i=1;i<=5;i++)
+{
 	System.out.println("Hello");
-
 }
-} 
+	System.out.println("Hi");
+
+for (int i=1;i<=5;i++)
+{
+	System.out.println("BIT");
+	if(i==2)
+	break;
+}
+	
+for (int i=1;i<=5;i++)
+System.out.println("i="+i);
+System.out.println("Have a good day");
+
+}}
