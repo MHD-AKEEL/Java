976633100cb604f5db2d8e0f59cda6e705240957
MHD-AKEEL <mhdakeel7755@gmail.com>
2025-10-07 00:38:27 +0300

    modified check.java

--- Java/check.java
+++ Java/check.java
@@ -1,13 +1,13 @@
 public class check{
 public static void main(String args[]){
 
-for (int i=0; i<=3; i++)
+int x = 3, result=4;
+switch(x+3)
 {
-	for (int k=0; k<=3; k++)
-	if (i<=k)
-	System.out.print("* ");
-	else
-	System.out.print(" ");
-	System.out.println();
+case 6: result = 0;
+case 7: result = 2;
+case 8: result = 5;
+default: result += 4;
 }
+System.out.println(result);
 }}
