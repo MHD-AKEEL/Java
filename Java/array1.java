8c427a7932bc4bde3609cb17ea683a222d72e849
MHD-AKEEL <mhdakeel7755@gmail.com>
2025-10-08 00:03:53 +0300

    created array1.java

---
+++ Java/array1.java
@@ -0,0 +1,13 @@
+public class array1{
+	public static void main(String args[]){
+
+	int marks[] =new int[6];
+
+	marks[0]=58;
+	marks[1]=54;
+	marks[2]=65;
+	marks[3]=42;	
+	marks[4]=38;
+	marks[5]=85;
+
+}}
