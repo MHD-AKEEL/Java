464aab58931aec24e73baac5c510131ccafd9342
MHD-AKEEL <mhdakeel7755@gmail.com>
2025-10-08 15:15:26 +0300

    modified array1.java

--- Java/array1.java
+++ Java/array1.java
@@ -1,11 +1,11 @@
 public class array1{
 	public static void main(String args[]){
 
-
+/*1 method
 	int[] marks={58,54,65,42,38,85};
 	
 	System.out.println(marks[2]);
-
+*/
 
 
 /* 2 method
@@ -20,5 +20,17 @@ int marks[] =new int[6];
 */
 
 
+int[] marks={45,5,65,77,98,44};
+
+String[] name={"anirudh", "rahman", "danush", "sana", "siva"};
+
+marks[0]=95;
+marks[1]=76;
+marks[2]=84;
+
+System.out.println(marks[1]);
+System.out.println(marks[5]);
+
+System.out.println(name[2]);
 
 }}
