95176a291bca4d3308351f15602ed94a7fe066d6
MHD-AKEEL <mhdakeel7755@gmail.com>
2025-10-14 22:59:15 +0300

    modified ex18.java

--- Java/ex18.java
+++ Java/ex18.java
@@ -4,7 +4,8 @@ public static void main(String args[]){
 int arr[]={10,20,30,40,50};
 for (int i=0; i<=arr.length; i++)
 {
-System.out.print(" " + arr[i]);
+System.out.println(" " + arr[i]);
+System.out.println(arr[2]);
 }	
 
 }}
