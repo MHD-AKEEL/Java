9b91ba2dcd596b346b7082feb8323c230c912559
MHD-AKEEL <mhdakeel7755@gmail.com>
2025-10-15 22:46:55 +0300

    created array2d.java

---
+++ Java/array2d.java
@@ -0,0 +1,12 @@
+public class array2d{
+public static void main(String args[]){
+
+String[] cars={"RAM","BMW","GMC"};
+
+int[][] myNumber={{1,2,3,4},{5,6,7}};
+
+int x=myNumber[1][2];
+
+System.out.println(x);
+
+}}
