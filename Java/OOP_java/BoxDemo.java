26ef429f7b91efda96aa02c8fce49a3c47ed8608
MHD-AKEEL <mhdakeel7755@gmail.com>
2025-11-25 15:23:33 +0300

    modified BoxDemo.java

--- Java/OOP_java/BoxDemo.java
+++ Java/OOP_java/BoxDemo.java
@@ -8,6 +8,7 @@ public class BoxDemo {
         Box my_box1 = new Box();
         Box my_box2 = new Box();
         Box my_box3 = new Box();
+        Box my_box4 = new Box();
 
         my_box1.width = 10;
         my_box1.height = 10;
@@ -21,9 +22,14 @@ public class BoxDemo {
         my_box3.height = 20;
         my_box3.length = 20;
 
+        my_box4.width = 5;
+        my_box4.height = 5;
+        my_box4.length = 5;
+
         double volume1;
         double Volume2;
         double Volume3;
+        double Volume4;
 
         volume1 = my_box1.width * my_box1.height * my_box1.length;
         System.out.println("Volume is = " + volume1);
@@ -33,5 +39,8 @@ public class BoxDemo {
 
         Volume3 = my_box3.width * my_box3.height * my_box3.length;
         System.out.println("Volume3 is = " + Volume3);
+
+        Volume4 = my_box4.width * my_box4.height * my_box4.length;
+        System.out.println("Volume4 is = " + Volume4);
     }
 }
