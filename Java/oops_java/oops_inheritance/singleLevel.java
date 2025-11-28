fd496bfb3af5abed1822729652d0ff4db413f3ca
MHD-AKEEL <mhdakeel7755@gmail.com>
2025-11-28 19:35:45 +0300

    creatd singleLevel.java

---
+++ Java/oops_java/oops_inheritance/singleLevel.java
@@ -0,0 +1,29 @@
+// Single level inheritance
+
+class A {
+
+    int a = 20;
+
+    void test() {
+        System.out.println("Hello");
+    }
+}
+
+class B extends A {
+
+    int b = 10;
+
+    void run() {
+        System.out.println("Java");
+    }
+}
+
+public class singleLevel {
+
+    public static void main(String[] args) {
+        B ref = new B();
+
+        ref.run();
+        ref.test();
+    }
+}
