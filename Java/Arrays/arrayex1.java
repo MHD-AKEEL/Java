dc0438680e205363aa381237741f1392ac7524ec
MHD-AKEEL <mhdakeel7755@gmail.com>
2025-12-21 22:18:27 +0300

    modified arrayex1.java

--- Java/Arrays/arrayex1.java
+++ Java/Arrays/arrayex1.java
@@ -5,8 +5,10 @@ float ar[] ={1, 2, 3, 4, 3.4f, 4.4f};
 	for(int i=0; i<6; i++)
 	System.out.println(ar[i]+" ");
 
-int[] num={10,20,30,45,55};
+int[] num={10,20,30,45,55,70};
 System.out.println(num[1]);
+System.out.println(num[3]);
+System.out.println(num[1]+num[3]);
 
 String[] name={"rizny", "alfi", "azar", "dinesh","kareem"};
 System.out.println(name[1]);
