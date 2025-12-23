6f16622aa2ad9d7dc3f77d8d88abc34a543fa29c
MHD-AKEEL <mhdakeel7755@gmail.com>
2025-12-23 08:35:55 +0300

    rename Java/Test_and_Check

---
+++ Java/Test_and_Check/BIT.java
@@ -0,0 +1,24 @@
+public class BIT {
+
+	public static void main(String[] args) {
+	System.out.println("Welcome to Java");
+
+
+float num2=12.0f;
+
+int num1=010;
+int num3=021;
+int num4=0b100;
+float num5=010f;
+
+int num6=0x10;
+
+
+
+System.out.println("num1="+num1);
+System.out.println("num3="+num3);
+System.out.println("num4="+num4);
+System.out.println("num5="+num5);
+System.out.println("num6="+num6);
+    }
+}
---
+++ Java/Test_and_Check/BIT2.java
@@ -0,0 +1,19 @@
+public class BIT2 {
+
+	public static void main(String[] args) {
+
+	System.out.println("My\nFirst\nJava\nProgramme");
+
+	System.out.println("\"Hi Everyone\"");
+
+	System.out.println("123456\rabc");
+
+	System.out.println("12\n3456\rabc");
+
+	System.out.println("1234\r56\nabc");
+
+	System.out.println("123\tabc");
+
+	
+}
+}
