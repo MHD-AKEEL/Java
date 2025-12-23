003f24abd84d952c6ee6d857b24c3c646020df8e
MHD-AKEEL <mhdakeel7755@gmail.com>
2025-12-23 21:56:15 +0300

    modified ex1.java

--- Java/Test_and_Check/ex1.java
+++ Java/Test_and_Check/ex1.java
@@ -3,11 +3,12 @@ public class ex1 {
         double i = 3.0, j = 3.0, k = 3.0;
         double x = 0.0, y = 2.3;
         
-        System.out.println(i<2 || k<2); // false OR operator
-        System.out.println(y>=2 && j<=3); // true AND operator
-        System.out.println(! (3>j)); // true NOT operator
+        System.out.println(i<2 || k<2); // false - OR operator
+        System.out.println(y>=2 && j<=3); // true - AND operator
+        System.out.println(! (3>j)); // true - NOT operator
 
-        System.out.println(x<=4 || y>=1); 
-        System.out.println(x>2 && y>=3);
+        System.out.println(x<=4 || y>=1); // true - OR operator
+        System.out.println(x>2 && y>=3); // false - AND operator
+        System.out.println(!(i>4)); // false - NOT operator
     }
 }
