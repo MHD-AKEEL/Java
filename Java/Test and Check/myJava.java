bc77605866eb5f3be1b713e6fe64c056a588bf6e
MHD-AKEEL <mhdakeel7755@gmail.com>
2025-12-23 08:05:07 +0300

    modified myJava.java

--- Java/Test and Check/myJava.java
+++ Java/Test and Check/myJava.java
@@ -3,11 +3,13 @@ public class myJava {
         int x=4 , y=2;
         System.out.println("x="+x); //x=4
         System.out.println("y="+y); //y=2
-        System.out.println("x+y="+(x+y)); //x+y=6
-        System.out.println("x+y="+x+y); //x+y=42
+        System.out.println("x+y="+(x+y)); //x+y=6 add pannum
+        System.out.println("x+y="+x+y); //x+y=42 conjenation aahum
         System.out.println("x-y="+(x-y)); //x-y=2
-        System.out.println("x%y="+x%y); //x%y=0
+        System.out.println("x%y="+(x%y)); //x%y=0
+        System.out.println("x*y="+x*y); //x*y=8 
         System.out.println("x*y="+(x*y)); //x*y=8
-        System.out.println("x/y="+x/y);
+        System.out.println("x/y="+(x/y)); //x/y=2
+        
     }
 }
