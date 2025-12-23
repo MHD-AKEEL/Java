75646305e75cbcff3f6215a057d8970acb9860f7
MHD-AKEEL <mhdakeel7755@gmail.com>
2025-12-23 08:38:08 +0300

    rename  Java/if_else_condition

---
+++ Java/if_else_condition/condition.java
@@ -0,0 +1,19 @@
+public class condition{
+
+public static void main(String args[]){
+
+int marks=70;
+
+if(marks>=50)
+{
+	System.out.println("pass");
+}
+else
+{
+	System.out.println("fail");
+	System.out.println("f1");
+	
+}
+	System.out.println("f2");
+}
+}
