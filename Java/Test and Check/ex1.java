c3b159b151b454ed28f31921f1eb86175ff6326c
MHD-AKEEL <mhdakeel7755@gmail.com>
2025-12-23 08:25:20 +0300

    created ex1.java

---
+++ Java/Test and Check/ex1.java
@@ -0,0 +1,10 @@
+public class ex1 {
+    public static void main(String[] args) {
+        double i = 3.0, j = 3.0, k = 3.0;
+        double x = 0.0, y = 2.3;
+        
+        System.out.println(i<2 || k<2); 
+        System.out.println(y>=2 && j<=3);
+        System.out.println(! (3>j));
+    }
+}
