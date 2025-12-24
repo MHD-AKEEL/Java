b455dd1a1bf880589687aa514fe5b99297f31e0f
MHD-AKEEL <mhdakeel7755@gmail.com>
2025-12-24 08:21:47 +0300

    created bitwise.java

---
+++ Java/Test_and_Check/bitwise.java
@@ -0,0 +1,13 @@
+public class bitwise {
+    public static void main(String[] args) {
+        int num1 = 5;
+        int num2 = 2;
+        int num3, num4;
+
+        num3= 7 & 2; // bitwise AND
+        System.out.println(num3); // output: 2
+
+        num4 = 7 | 2; // bitwise OR
+        System.out.println(num4); // output: 7
+    }
+}
