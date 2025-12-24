39603f82caeaf0ca15f8ffef05e6c4046731ede9
MHD-AKEEL <mhdakeel7755@gmail.com>
2025-12-24 07:41:57 +0300

    created ex2.java

---
+++ Java/Test_and_Check/ex2.java
@@ -0,0 +1,10 @@
+public class ex2 {
+    public static void main(String[] args){
+       double i = 4, j = 6.0, k=1.0;
+       double x=2.0, y=4.3;
+       
+       System.out.println(i<2 && k<2);
+       System.out.println(y>=2 || j<=3);
+       System.out.println(!(3>x));
+    }
+}
