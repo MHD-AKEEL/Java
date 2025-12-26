9765feeae02fa682314e781cffff8fa701dfc116
MHD-AKEEL <mhdakeel7755@gmail.com>
2025-12-26 19:28:18 +0300

    modified calc.java

--- Java/Test_and_Check/calc.java
+++ Java/Test_and_Check/calc.java
@@ -1,6 +1,8 @@
 public class calc {
     public static void main(String[] args) {
         int num1 = 0b100;
+        int num2 = 0x10;
         System.out.println("num1=" + num1);
+        System.out.println("num2=" + num2);
     }
 }
