ce2cedc3d801f931e25fbbc355516ce22dbd14e8
MHD-AKEEL <mhdakeel7755@gmail.com>
2025-12-26 20:30:20 +0300

    modified calc.java

--- Java/Test_and_Check/calc.java
+++ Java/Test_and_Check/calc.java
@@ -5,8 +5,10 @@ public class calc {
         System.out.println("num1=" + num1);
         System.out.println("num2=" + num2);
 
-        System.out.println("java\n programmig\b language\n");
-        System.out.println("my\nfirst\\javaprogram\n");
-        System.out.println("123456\rabc");
+        System.out.println("java\n programmig\b language\n"); // new line and backspace
+        System.out.println("my\nfirst\\javaprogram\n"); // new line and backslash
+        System.out.println("123456\rabc\n"); // carriage return
+        System.out.println("She said \"Hello World\"\n"); // double quotes 
+        System.out.println("Hi\teveryone"); // Tab space
     }
 }
