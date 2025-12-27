02c68cdb594c47d6d249c77da05ca4fe159f9198
MHD-AKEEL <mhdakeel7755@gmail.com>
2025-12-27 14:40:11 +0300

    created calc2.java

---
+++ Java/Test_and_Check/calc2.java
@@ -0,0 +1,10 @@
+public class calc2 {
+    public static void main(String[] arrg) {
+        int num1 =5;
+        int num2 =2;
+        int num3 =7;
+
+        System.out.println(num1>8 & num2>1); //false
+        System.out.println(num1>8 | num2>1); //true
+    }
+}
