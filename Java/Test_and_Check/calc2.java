0b4579c802ff65dc7cc39b9849816100094f9f0a
MHD-AKEEL <mhdakeel7755@gmail.com>
2025-12-27 15:19:20 +0300

    modified calc2.java

--- Java/Test_and_Check/calc2.java
+++ Java/Test_and_Check/calc2.java
@@ -4,8 +4,14 @@ public class calc2 {
         int num2 =2;
         int num3 =7;
 
-        System.out.println(num1>8 & num2>1); //false
-        System.out.println(num1>8 | num2>1); //true
-        System.out.println(!(num3>5)); //false
+        System.out.println(num1>8 & num2>1); //bitwise AND = false
+        System.out.println(num1>8 | num2>1); //bitwise OR = true
+        System.out.println(!(num3>5)); //NOT operator = false
+
+        float num4 = 10;
+        float num5;
+        num5 = num4/3;
+        System.out.println(num5); // print 3.3333333
+
     }
 }
