eae05e4de4de9d32d5c58c9466f00de80bb342e4
MHD-AKEEL <mhdakeel7755@gmail.com>
2025-12-28 08:23:01 +0300

    created calc3.java

---
+++ Java/Test_and_Check/calc3.java
@@ -0,0 +1,11 @@
+public class calc3 {
+    public static void main(String[] args){
+        int num1 = 25;
+        int num2 = 40;
+        char value1 = (char)(num1 + num2); // 25 + 40 = 65 -> 'A'
+        System.out.println(value1);
+
+
+        
+    }
+}
