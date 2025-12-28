367835bee10d42e62ce1a1172a01e337cbcdb7d0
MHD-AKEEL <mhdakeel7755@gmail.com>
2025-12-28 08:30:40 +0300

    modified calc3.java

--- Java/Test_and_Check/calc3.java
+++ Java/Test_and_Check/calc3.java
@@ -3,11 +3,15 @@ public class calc3 {
         int num1 = 25;
         int num2 = 40;
         char value1 = (char)(num1 + num2); // 25 + 40 = 65 -> 'A'
+
         System.out.println(value1);
 
         char ch1 = 'A';
         char ch2 = 'B';
+        char ch3 = ch1 * ch2; // error: incompatible types: possible lossy conversion from int to char
+
         System.out.println(ch1 + ch2); // 'A' + 'B' = 65 + 66 = 131
+        System.out.println(ch3);
 
         
     }
