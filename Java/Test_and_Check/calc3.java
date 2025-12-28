0902ef5af01a8d198b98c4f108c9a75c7ee9f17c
MHD-AKEEL <mhdakeel7755@gmail.com>
2025-12-28 08:23:57 +0300

    modified calc3.java

--- Java/Test_and_Check/calc3.java
+++ Java/Test_and_Check/calc3.java
@@ -5,6 +5,9 @@ public class calc3 {
         char value1 = (char)(num1 + num2); // 25 + 40 = 65 -> 'A'
         System.out.println(value1);
 
+        char ch1 = 'A';
+        char ch2 = 'B';
+        System.out.println(ch1 + ch2); // 'A' + 'B' = 65 + 66 = 131
 
         
     }
