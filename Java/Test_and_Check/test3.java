ca5e81268a03cb5bae5e92850ccacf136b953eac
MHD-AKEEL <mhdakeel7755@gmail.com>
2025-12-31 23:15:16 +0300

    modified test3.java

--- Java/Test_and_Check/test3.java
+++ Java/Test_and_Check/test3.java
@@ -1,13 +1,13 @@
 public class test3 {
-    public static void main(String[] args) {
-        System.out.println("in first main()");
+    public static void main(String[] args) { // original main method
+        System.out.println("in first main()"); // changed output to differentiate
     }
 
-    public static void main(int[] args) {
+    public static void main(int[] args) { // changed to int[] to avoid duplicate method signature
         System.out.println("a");
     }
 
-    public static void main(char[] args) {
+    public static void main(char[] args) {//  changed to char[] to avoid duplicate method signature
         System.out.println("a");
     }
 }
