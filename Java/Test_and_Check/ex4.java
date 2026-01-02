15c382d5f36e623dc5736760d15826f8f4547092
MHD-AKEEL <mhdakeel7755@gmail.com>
2026-01-02 21:58:52 +0300

    created ex4.java

---
+++ Java/Test_and_Check/ex4.java
@@ -0,0 +1,12 @@
+public class ex4 {
+    public static void main(String[] args) {
+       float value1 = 2.0f;
+       int num1 = 10;
+       String str = "100";
+       char ch = 'B';
+
+         System.out.println(str + ch);
+         System.out.println(ch + value1);
+         
+    }
+}
