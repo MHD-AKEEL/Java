f88e69b1469fc129f6895c4298ce39bcf7ed250b
MHD-AKEEL <mhdakeel7755@gmail.com>
2026-01-03 07:26:01 +0300

    created ex5.java

---
+++ Java/Test_and_Check/ex5.java
@@ -0,0 +1,10 @@
+public class ex5 {
+    public static void main(String[] args) {
+       final int value;
+        value = 7;
+        System.out.println(value);
+
+        char a = '\\';
+        System.out.println("C:" + a + "TC" + a + "BIN");
+    }
+}
