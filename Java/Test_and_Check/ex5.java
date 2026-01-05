140ad413e672942bdf546d3d0b8f56453687fee1
MHD-AKEEL <mhdakeel7755@gmail.com>
2026-01-05 22:33:14 +0300

    modified ex5.java

--- Java/Test_and_Check/ex5.java
+++ Java/Test_and_Check/ex5.java
@@ -2,9 +2,9 @@ public class ex5 {
     public static void main(String[] args) {
        final int value;
         value = 7;
-        System.out.println(value);
+        System.out.println(value); //print 7
 
-        char a = '\\';
+        char a = '\\'; //escape character
         System.out.println("C:" + a + "TC" + a + "BIN");//print C:\TC\BIN
     }
 }
