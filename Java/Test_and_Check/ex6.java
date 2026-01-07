46c02531171d7663dcecdb323bdaafb55b66657c
MHD-AKEEL <mhdakeel7755@gmail.com>
2026-01-07 22:01:30 +0300

    modified test.java

--- Java/Test_and_Check/ex6.java
+++ Java/Test_and_Check/ex6.java
@@ -1,11 +1,11 @@
 public class ex6 {
     public static void main(String[] args) {
         double a,b,c;
-        a= 3.0/0;
-        b= 0.0/4.0;
-        c= 0.0/0.0;
+        a= 3.0/0; // Infinity
+        b= 0.0/4.0; // 0.0
+        c= 0.0/0.0; // NaN
         System.out.println(a); // Infinity
-        System.out.println(b);
-        System.out.println(c);
+        System.out.println(b); // 0.0
+        System.out.println(c); // NaN
     }
 }
--- Java/Test_and_Check/test.java
+++ Java/Test_and_Check/test.java
@@ -11,18 +11,18 @@ int num1=5;
 int num2=2;
 int num3=7;
 
-System.out.println(num1>8 && num2>1);
-System.out.println(num1>8 || num2>1);	
+System.out.println(num1>8 && num2>1); // false
+System.out.println(num1>8 || num2>1); // true	
 
-System.out.println(num1>8 & num2>1);
-System.out.println(num1>8 | num2>1);
-System.out.println(num2>8 || num3>3);
+System.out.println(num1>8 & num2>1); // false
+System.out.println(num1>8 | num2>1); // true
+System.out.println(num2>8 || num3>3); // true
 
-	System.out.println(c);
-	System.out.println(d);
+	System.out.println(c); // 4
+	System.out.println(d); // 5
 
 
-System.out.println(num3);
+System.out.println(num3); // 7
 }
 
 }
