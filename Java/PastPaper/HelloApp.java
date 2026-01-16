d0d3617d4a97602038145dbc75a3099979742eb4
MHD-AKEEL <mhdakeel7755@gmail.com>
2026-01-16 16:32:24 +0300

    created HelloApp

---
+++ Java/PastPaper/HelloApp.java
@@ -0,0 +1,14 @@
+
+class HelloApp {
+
+    public static void main(String[] args) {
+        char ch = 'A';
+        int count = 0;
+        while (count < 5) {
+            System.out.print(ch);
+            ch += 2;
+            count++;
+        }
+    }
+}
+
