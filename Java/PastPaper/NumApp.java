40560b66e8ed9234c5dd824c6a626ffb0a9972e2
MHD-AKEEL <mhdakeel7755@gmail.com>
2026-01-16 16:12:14 +0300

    created PastPaper

---
+++ Java/PastPaper/NumApp.java
@@ -0,0 +1,15 @@
+public class NumApp{
+
+    public static void main(String[] args) {
+        int val1, val2;
+        String val3, val4, val5;
+        val1 = 5;
+        val2 = 2;
+        val2 *= 3;
+        val3 = Integer.toString(val1);
+        val4 = Integer.toString(val2);
+        val5 = val3 + val4;
+        System.out.println(val5);
+    }
+}
+
