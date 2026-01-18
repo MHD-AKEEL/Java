3971091fe8fda8ce55493934bd8bc5987aca6bb3
MHD-AKEEL <mhdakeel7755@gmail.com>
2026-01-18 07:21:13 +0300

    modified IndexApp.java

--- Java/PastPaper/IndexApp.java
+++ Java/PastPaper/IndexApp.java
@@ -3,23 +3,13 @@ public class IndexApp {
 
     public static void main(String[] args) {
         outer:
-        for ( int i = 0;i< 2; i++
-
-        ) {
-for ( int j = 0;j< 2; j++
-                
-            ) {
-System.out.print(
-                " i =" + i + "j =" + j +
-                " , ");
-if ( i == 0 && j == 1
-
-                )
-{
-break outer;
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < 2; j++) {
+                System.out.print(" i =" + i + "j =" + j + " , "); 
+                if (i == 0 && j == 1) {
+                    break outer;
                 }
             }
         }
     }
 }
-
