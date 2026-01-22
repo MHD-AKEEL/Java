a8241fd5861e17f9b38fb0fcacd64f76e0b01df6
MHD-AKEEL <mhdakeel7755@gmail.com>
2026-01-22 19:45:06 +0300

    modified NumApp.java

--- Java/PastPaper/NumApp.java
+++ Java/PastPaper/NumApp.java
@@ -3,14 +3,14 @@ public class NumApp {
         int val1, val2;
         String val3, val4, val5;
         
-        val1 = 5;
+        val1 = 5; 
         val2 = 2;
         val2 *= 3;
-        val3 = Integer.toString(val1);
-        val4 = Integer.toString(val2);
-        val5 = val3 + val4;
+        val3 = Integer.toString(val1); // output "5"
+        val4 = Integer.toString(val2); // output "6"
+        val5 = val3 + val4; 
         
-        System.out.println(val5);
+        System.out.println(val5); // output "56"
     }
 }
 
